a64ff683826ab9d2deb5d0a5d4a7f6deec6f939c
IamQui <iamquicode@gmail.com>
2024-11-25 17:36:31 -0600

    debugging-scripts

--- foundry-fund-me/test/FundMeTest.t.sol
+++ foundry-fund-me/test/FundMeTest.t.sol
@@ -16,4 +16,9 @@ contract FundMeTest is Test{
     function testMinimumDollarIsFive() public view {
         assertEq(fundMe.MINIMUM_USD(), 5e18);
     }
+
+    function testOwnerIsMsgSender() public view {
+        console.log(address(this));
+        assertEq(fundMe.i_owner(), address(this));
+    }
 }
