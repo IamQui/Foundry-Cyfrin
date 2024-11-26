742954ba30991d860c9ad5e97c349f4701852c87
IamQui <iamquicode@gmail.com>
2024-11-26 15:26:41 -0600

    refactoring-contracts-and-tests

--- foundry-fund-me/test/FundMeTest.t.sol
+++ foundry-fund-me/test/FundMeTest.t.sol
@@ -3,14 +3,21 @@ pragma solidity ^0.8.26;
 
 import {Test, console} from "forge-std/Test.sol";
 import {FundMe} from "../src/FundMe.sol";
+import {DeployFundMe} from "../script/DeployFundMe.s.sol";
 
 contract FundMeTest is Test{
     FundMe fundMe;
         
     function setUp() external{
-        fundMe = new FundMe();
+        //fundMe = new FundMe();
         // our fundMe(variable) of type FundMe
         // is going to be a new FundMe() contract
+
+        DeployFundMe deployFundMe = new DeployFundMe();
+        fundMe = deployFundMe.run();
+
+
+
     }
 
     function testMinimumDollarIsFive() public view {
@@ -19,7 +26,7 @@ contract FundMeTest is Test{
 
     function testOwnerIsMsgSender() public view {
         console.log(address(this));
-        assertEq(fundMe.i_owner(), address(this));
+        assertEq(fundMe.i_owner(), msg.sender);
     }
 
     // What can we do to work with addresses outside our system?
