bc7c4e92194855a51165627a72a008c9b6c6f3e0
IamQui <iamquicode@gmail.com>
2024-11-26 15:05:34 -0600

    tests-on-chain-forks

---
+++ foundry-fund-me/notes.md
@@ -0,0 +1,9 @@
+source .env
+echo nameOfFile
+
+ex:
+source .env
+echo $SEPOLIA_RPC_URL
+
+returns:
+https://eth-sepolia.g.alchemy.com/v2/GHd9LC0UqmA7VnXafXnAMkyTZ0J9P-dz
--- foundry-fund-me/test/FundMeTest.t.sol
+++ foundry-fund-me/test/FundMeTest.t.sol
@@ -21,4 +21,25 @@ contract FundMeTest is Test{
         console.log(address(this));
         assertEq(fundMe.i_owner(), address(this));
     }
+
+    // What can we do to work with addresses outside our system?
+
+    // 4 DIFFERENT TYPES OF TESTS
+
+    // 1. Unit
+    //    - Testing a specific part of our code
+
+    // 2. Integration
+    //    - Testing how our code works with other parts of our code
+
+    // 3. Forked
+    //    - Testing our code on a simulated real environment
+
+    // 4. Staging
+    //    - Testing our code in a real environment that is not a production environment
+
+    function testPriceFeedVersionIsAccurate() public view {
+        uint256 version = fundMe.getVersion();
+        assertEq(version, 4);
+    }
 }
