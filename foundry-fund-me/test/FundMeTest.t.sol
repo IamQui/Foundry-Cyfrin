551a0a4f20f1023860f296aeca3043c17f00d110
IamQui <iamquicode@gmail.com>
2024-11-27 02:07:10 -0600

    withdraw-gas-costs

--- foundry-fund-me/test/FundMeTest.t.sol
+++ foundry-fund-me/test/FundMeTest.t.sol
@@ -11,6 +11,7 @@ contract FundMeTest is Test{
     address USER = makeAddr("user");
     uint256 constant SEND_VALUE = 0.1 ether;
     uint256 constant STARTING_BALANCE = 10 ether;
+    uint256 constant GAS_PRICE = 1;
         
     function setUp() external{
         //fundMe = new FundMe();
